54c1a008faf3ad9a777087a09df5d7223781a96a
seanng <shonum@gmail.com>
2017-01-24 15:08:25 +0800

    Add datepicker and show/hide divs on job type select

--- js/createpost.js
+++ js/createpost.js
@@ -1,15 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function() { init() });
 
-  $('.dropdown li a').click(function(e){
-    e.preventDefault();
-    $('.btn:first-child').text($(this).text());
-    $('.btn:first-child').val($(this).text());
+function init () {
+  eventHandlers();
+  initDatePicker();
+}
+
+function eventHandlers () {
+  $('.dropdown li a').off().on('click', selectJobCategoryHandler);
+  $('label').eq(0).off().on('click', selectTempHandler);
+  $('label').eq(1).off().on('click', selectPermHandler);
+}
+
+function selectJobCategoryHandler (e) {
+  $('.btn:first-child').text($(this).text());
+  $('.btn:first-child').val($(this).text());
+}
+
+function selectTempHandler (e) {
+  $('.date-and-times')
+    .css('visibility', 'visible')
+    .css('max-height', '1000px')
+    .css('margin-bottom', '15px');
+}
+
+function selectPermHandler (e) {
+  $('.date-and-times')
+    .css('visibility', 'hidden')
+    .css('max-height', '0px')
+    .css('margin-bottom', '0px');
+}
+
+function initDatePicker () {
+  $('.datepicker-here').eq(0).datepicker({
+    minDate: new Date(),
+    language: 'en'
   })
-
-
-  function init () {
-
-  }
-
-  init();
-})
+}
